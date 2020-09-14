7288eec4e3af808c0fbc954f2f14a0051d22d428
nitro-neal <groessler24@gmail.com>
2020-09-13 20:56:50 -0500

    :coffee: Adding coffee UI

---
+++ src/components/Footer/components/Nav.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Nav: React.FC = () => {
+  return (
+    <StyledNav>
+      <StyledLink
+        target="_blank"
+        href="https://etherscan.io/address/0xdd926275b61f65c40f4d5b34331cdab9dc5dcf97#code"
+      >
+        Barista Contract
+      </StyledLink>
+      <StyledLink
+        target="_blank"
+        href="https://uniswap.info/pair/0xce84867c3c02b05dc570d0135103d3fb9cc19433"
+      >
+        Uniswap COFFEE-ETH
+      </StyledLink>
+      <StyledLink target="_blank" href="https://google.com">
+        Discord
+      </StyledLink>
+      <StyledLink target="_blank" href="https://google.com">
+        Github
+      </StyledLink>
+      <StyledLink target="_blank" href="https://google.com">
+        Twitter
+      </StyledLink>
+    </StyledNav>
+  )
+}
+
+const StyledNav = styled.nav`
+  align-items: center;
+  display: flex;
+`
+
+const StyledLink = styled.a`
+  color: ${(props) => props.theme.color.grey[400]};
+  padding-left: ${(props) => props.theme.spacing[3]}px;
+  padding-right: ${(props) => props.theme.spacing[3]}px;
+  text-decoration: none;
+  &:hover {
+    color: ${(props) => props.theme.color.grey[500]};
+  }
+`
+
+export default Nav
